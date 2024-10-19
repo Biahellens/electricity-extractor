7339944ebf67b0e65664b88859a666e2945bc792
Bianca Hellen <60991740+Biahellens@users.noreply.github.com>
2024-10-19 17:01:30 -0300

    :recycle: refactor: Adicionando correções nas migrations e no schema

--- server/prisma/migrations/20241019184124_init/migration.sql
+++ server/prisma/migrations/20241019184124_init/migration.sql
@@ -6,10 +6,12 @@ CREATE TABLE "fatura_energia" (
     "energiaEletricaQuantidade" DOUBLE PRECISION NOT NULL,
     "energiaEletricaValor" DOUBLE PRECISION NOT NULL,
     "energiaSCEEEQuantidade" DOUBLE PRECISION NOT NULL,
+    "energiaSCEEEValor" DOUBLE PRECISION NOT NULL,
+    "energiaCompensadaQuantidade" DOUBLE PRECISION NOT NULL,
     "energiaCompensadaValor" DOUBLE PRECISION NOT NULL,
     "contribIlumPM" DOUBLE PRECISION NOT NULL,
     "pdfFatura" TEXT NOT NULL,
-    "valotTotal" DOUBLE PRECISION NOT NULL,
+    "valorTotal" DOUBLE PRECISION NOT NULL,
 
     CONSTRAINT "fatura_energia_pkey" PRIMARY KEY ("id")
 );
