09719f66865d4a9cca19edbfebb5097206d82f01
Bianca Hellen <60991740+Biahellens@users.noreply.github.com>
2024-10-19 15:07:13 -0300

    :sparkles: feat: Criando schema e migration com o prisma

---
+++ server/prisma/migrations/20241019180246_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "fatura_energia" (
+    "id" SERIAL NOT NULL,
+    "nCliente" TEXT NOT NULL,
+    "mesRef" TEXT NOT NULL,
+    "energiaEletricaQuantidade" DOUBLE PRECISION NOT NULL,
+    "energiaEletricaValor" DOUBLE PRECISION NOT NULL,
+    "energiaSCEEEQuantidade" DOUBLE PRECISION NOT NULL,
+    "energiaCompensadaValor" DOUBLE PRECISION NOT NULL,
+    "contribIlumPM" DOUBLE PRECISION NOT NULL,
+    "pdfFatura" TEXT NOT NULL,
+
+    CONSTRAINT "fatura_energia_pkey" PRIMARY KEY ("id")
+);
